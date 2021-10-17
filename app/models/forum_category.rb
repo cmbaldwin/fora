78c25963abd1d1e1ff3e3afeed8e35215b3152bd
Cody Baldwin <codybaldwin@gmail.com>
2021-10-17 09:14:52 +0900

    Update forum_category.rb

--- app/models/forum_category.rb
+++ app/models/forum_category.rb
@@ -5,7 +5,9 @@ class ForumCategory < ApplicationRecord
   scope :sorted, -> { order(name: :asc) }
 
   validates :name, :slug, :color, presence: true
-
+  validates :slug, uniqueness: true
+  validates :name, uniqueness: true
+  
   def color
     colour = super
     colour.start_with?("#") ? colour : "##{colour}"
